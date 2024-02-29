703b0149782c317c1f50a9aca0dd13dd7701fb01
Pranjal Sharma <pranjal.software.engineer@gmail.com>
2024-02-29 14:06:33 +0530

    "Modular Approch In Python"

---
+++ Python/Python_Project_env/app.py
@@ -0,0 +1,10 @@
+from src.person import Person
+
+
+def main():
+    person = Person("Alice", 25)
+    person.display()
+
+
+if __name__ == "__main__":
+    main()
---
+++ Python/Python_Project_env/readme.md
@@ -0,0 +1,2 @@
+## Modular Programing in Python
+
